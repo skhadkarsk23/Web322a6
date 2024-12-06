6ce0751a35494b1d62e042f06da9e6cd1f248e9a
Saurab Khadka <150497452+skhadkarsk23@users.noreply.github.com>
2024-12-06 15:27:17 +0000

    a6

--- server.js
+++ server.js
@@ -45,12 +45,12 @@ app.use((req, res, next) => {
 
 app.use(
   clientSessions({
-    cookieName: 'session', // this is the object name that will be added to 'req'
-    secret: 'o6LjQ5EVNC28ZgK64hDELM18ScpFQr', // this should be a long un-guessable string.
-    duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
-    activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
+    cookieName: "session", // Cookie name
+    secret: process.env.SESSION_SECRET, // Use environment variable
+    duration: 2 * 60 * 1000, // 2 minutes
+    activeDuration: 1000 * 60, // 1 minute
   })
-)
+);
 
 app.use((req, res, next) => {
   res.locals.session = req.session
@@ -132,6 +132,7 @@ app.get("/userHistory", ensureLogin, (req, res) => {
 app.get("/", (req, res) => {
   res.render("home")
 })
+
 
 app.get("/about", (req, res) => {
   res.render("about")
